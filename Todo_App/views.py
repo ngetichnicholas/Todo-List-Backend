60bdee11d1c45c4e88ec52dbb3733a7ec1f7e732
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-11-06 20:46:47 +0300

    Add view to list todo,to add and show single todo details

--- Todo_App/views.py
+++ Todo_App/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import TodoSerializer
+from .models import TodoNote
 
 # Create your views here.
+@api_view(['GET'])
+def index(request):
+    todo = TodoNote.objects.all()
+    serializer = TodoSerializer(todo,many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def todo_detail(request,pk):
+    todo = TodoNote.objects.get(id=pk)
+    serializer = TodoSerializer(todo,many = False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def add_todo(request):
+    serializer = TodoSerializer(data = request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
