baba56cd5c3a61b98f3456a07610952559123caa
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-11-06 19:39:30 +0300

    Add models for TodoNote and Category

--- Todo_App/models.py
+++ Todo_App/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=144)
+    class Meta:
+        verbose_name = ("Category")
+        verbose_name_plural = ("Categories")
+        
+    def __str__(self):
+        return self.name
+    
+class TodoNote(models.Model):
+    title = models.CharField(max_length=250)
+    note = models.TextField(blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_due = models.DateTimeField()
+    complete = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, default="general")
+    
+    class Meta:
+        ordering = ["-date_created"]
+        
+    def __str__(self):
+        return self.title
