8f0ba4216b8a66a8d7317a037e9616470cd13285
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-11-07 22:52:51 +0300

    Add test case for getting all todo list

--- Todo_App/tests.py
+++ Todo_App/tests.py
@@ -1,5 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 from .models import TodoNote,Category
+import json
+from rest_framework import status
+from django.urls import reverse
+from .serializers import TodoSerializer
+
+# initialize the APIClient app
+client = Client()
 
 # Create your tests here.
 class TodoNoteTest(TestCase):
@@ -28,3 +35,26 @@ class TodoNoteTest(TestCase):
             category.name, 'Testing'
         )
         
+class GetAllTodoTest(TestCase):
+    """ Test module for GET all Todo API """
+
+    def setUp(self):
+        test_category=Category.objects.create(name='Testing')
+        
+        TodoNote.objects.create(
+            title='Back end', note='Create Django Application', date_created='2021-11-07T20:52:26.727495+03:00',date_due='2021-11-07T20:52:26.727495+03:00',complete=False,category=test_category)
+        
+        TodoNote.objects.create(
+            title='New Test', note='New testing note', date_created='2021-11-07T20:52:26.727495+03:00',date_due='2021-11-07T20:52:26.727495+03:00',complete=False,category=test_category)
+        TodoNote.objects.create(
+            title='Hello Test', note='Hello world test case', date_created='2021-11-07T20:52:26.727495+03:00',date_due='2021-11-07T20:52:26.727495+03:00',complete=True,category =test_category)
+
+
+    def test_get_all_todo(self):
+        # get API response
+        response = client.get(reverse('index'))
+        # get data from db
+        todo = TodoNote.objects.all()
+        serializer = TodoSerializer(todo, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
