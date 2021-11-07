01280fb5bce6536289c38296f59929fb42d28ebe
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-11-07 23:29:28 +0300

    Add test case for geting single todo

--- Todo_App/tests.py
+++ Todo_App/tests.py
@@ -58,3 +58,31 @@ class GetAllTodoTest(TestCase):
         serializer = TodoSerializer(todo, many=True)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class GetSingleTodoTest(TestCase):
+    """ Test module for GET single Todo API """
+
+    def setUp(self):
+        test_category=Category.objects.create(name='Testing')
+        
+        self.back_end=TodoNote.objects.create(
+            title='Back end', note='Create Django Application', date_created='2021-11-07T20:52:26.727495+03:00',date_due='2021-11-07T20:52:26.727495+03:00',complete=False,category=test_category)
+        
+        self.new_test=TodoNote.objects.create(
+            title='New Test', note='New testing note', date_created='2021-11-07T20:52:26.727495+03:00',date_due='2021-11-07T20:52:26.727495+03:00',complete=False,category=test_category)
+        self.hello_test=TodoNote.objects.create(
+            title='Hello Test', note='Hello world test case', date_created='2021-11-07T20:52:26.727495+03:00',date_due='2021-11-07T20:52:26.727495+03:00',complete=True,category =test_category)
+
+    def test_get_valid_single_todo(self):
+        response = client.get(
+            reverse('todo_details', kwargs={'pk': self.back_end.pk}))
+        todo = TodoNote.objects.get(pk=self.back_end.pk)
+        serializer = TodoSerializer(todo)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_invalid_single_todo(self):
+        response = client.get(
+            reverse('todo_details', kwargs={'pk': 30}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
