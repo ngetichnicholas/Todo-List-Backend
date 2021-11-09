fe59b2ac3c9b5bac7727ec706d16936ac7084629
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-11-09 21:22:40 +0300

    Update serializers

--- Todo_App/serializers.py
+++ Todo_App/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import fields, serializers
 from .models import TodoNote
 
 class TodoSerializer(serializers.ModelSerializer):
+    date_due=serializers.DateField(format=None,input_formats=None)
     class Meta:
         model = TodoNote
         fields = '__all__'
